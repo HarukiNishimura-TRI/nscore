8e24272504d4526e7b1170983131ecbd0439e807
David Snyder <david.snyder.pi@tri.global>
2026-02-10 13:00:43 -0500

    Remove base because it's within sbt package; update utils __init__ accordingly

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -1,3 +1,3 @@
-from .. import Decision, Hypothesis, TestResult
+# from sequentialized_barnard_tests.base import Decision, Hypothesis, TestResult
 from .utils_general import *
 from .utils_wsr import *
