eede521b025043065ea7e0737254868b0dd635ca
Haruki Nishimura <haruki.nishimura@tri.global>
2026-09-02 11:13:38 -0700

    version updated to 0.1.1. in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="nscore",
-    version="0.1.0",
+    version="0.1.1",
     description="Sequential statistical hypothesis testing for generalized performance measures.",
     author="David Snyder, Haruki Nishimura",
     author_email="dsnyder5@engineering.upenn.edu, haruki.nishimura@tri.global",
